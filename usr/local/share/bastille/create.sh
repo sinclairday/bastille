0065a6b9f01cd39cef887b0aa04101c7d6e3e70e
Barry McCormick <bmccormick@debianbeast.maildragon.com>
2023-10-17 12:35:25 -0700

    slash check added

--- usr/local/share/bastille/create.sh
+++ usr/local/share/bastille/create.sh
@@ -82,6 +82,11 @@ validate_ip() {
         else
             local IFS
             if echo "${ip}" | grep -Eq '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))?$'; then
+		CHARACTER="/"
+		TEST_SLASH=$(echo "${ip}"
+		    if echo $TEST_SLASH | grep -q "$CHARACTER"
+                    else
+	                exit 1
 		TEST_MASK=$(echo "${ip}" | cut -d / -f2)
 		    if TEST_MASK=""
 			echo "missing netmask"
